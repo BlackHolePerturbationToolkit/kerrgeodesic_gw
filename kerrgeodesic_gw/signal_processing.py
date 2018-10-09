96a2f9a034bd66ae6f96d4f0eab5b26fecf5f96c
Eric Gourgoulhon <eric.gourgoulhon@obspm.fr>
2018-10-09 23:23:27 +0200

    Add doctests in signal processing

--- kerrgeodesic_gw/signal_processing.py
+++ kerrgeodesic_gw/signal_processing.py
@@ -29,12 +29,62 @@ def fourier(signal):
     INPUT:
 
     - ``signal``: list of pairs `(t, h(t))`, where `t` is the time and `h(t)`
-      is the signal at `t`.
+      is the signal at `t`. *NB*: the sampling in `t` must be uniform
 
     OUTPUT:
 
     - a numerical approximation (FFT) of the Fourier transform, as a list
       of pairs `(f,\tilde{h}(f))`
+
+    EXAMPLES:
+
+    Fourier transform of the `h_+` signal from a particle orbiting at the
+    ISCO of a Schwarzschild black hole::
+
+        sage: from kerrgeodesic_gw import h_particle_signal, fourier
+        sage: a, r0 = 0., 6.
+        sage: theta, phi = pi/2, 0
+        sage: sig = h_particle_signal(a, r0, theta, phi, 0., 800., nb_points=200)
+        sage: sig[:5]  # tol 1.0e-13
+        [(0.000000000000000, 0.1536656546005028),
+         (4.02010050251256, 0.03882960191740132),
+         (8.04020100502512, -0.0791773803745108),
+         (12.0603015075377, -0.18368354016995483),
+         (16.0804020100502, -0.25796165580196795)]
+        sage: ft = fourier(sig)
+        sage: ft[:5]   # tol 1.0e-13
+        [(-0.12437500000000001, (1.0901773159466113+0j)),
+         (-0.12313125000000001, (1.0901679813096925+0.021913447210468399j)),
+         (-0.12188750000000001, (1.0901408521337845+0.043865034665617614j)),
+         (-0.12064375000000001, (1.0900987733011678+0.065894415355341171j)),
+         (-0.11940000000000001, (1.0900473072498438+0.088044567109304195j))]
+
+    Plot of the norm of the Fourier transform::
+
+        sage: ft_norm = [(f, abs(hf)) for (f, hf) in ft]
+        sage: line(ft_norm, axes_labels=[r'$f M$', r'$|\tilde{h}_+(f)| r/(\mu M)$'],
+        ....:      gridlines=True, frame=True, axes=False)
+        Graphics object consisting of 1 graphics primitive
+
+    .. PLOT::
+
+        from kerrgeodesic_gw import h_particle_signal, fourier
+        a, r0 = 0., 6.
+        theta, phi = pi/2, 0
+        sig = h_particle_signal(a, r0, theta, phi, 0., 800., nb_points=200)
+        ft = fourier(sig)
+        ft_norm = [(f, abs(hf)) for (f, hf) in ft]
+        g = line(ft_norm, \
+                 axes_labels=[r'$f M$', r'$|\tilde{h}_+(f)| r/(\mu M)$'], \
+                 gridlines=True, frame=True, axes=False)
+        sphinx_plot(g)
+
+    The first peak is at the orbital frequency of the particle::
+
+        sage: f0 = n(1/(2*pi*r0^(3/2))); f0
+        0.0108291222393566
+
+    while the highest peak is at twice this frequency (`m=2` mode).
 
     """
     t = [s[0] for s in signal]
@@ -58,6 +108,26 @@ def read_signal(filename, dirname=None):
 
     - signal as a list of pairs `(t, h(t))`
 
+    EXAMPLES::
+
+        sage: from kerrgeodesic_gw import save_signal, read_signal
+        sage: sig0 = [(RDF(i/10), RDF(sin(i/5))) for i in range(5)]
+        sage: from sage.misc.temporary_file import tmp_filename
+        sage: filename = tmp_filename(ext='.dat')
+        sage: save_signal(sig0, filename)
+        sage: sig = read_signal(filename)
+        sage: sig   # tol 1.0e-13
+        [(0.0, 0.0),
+         (0.1, 0.19866933079506122),
+         (0.2, 0.3894183423086505),
+         (0.3, 0.5646424733950354),
+         (0.4, 0.7173560908995228)]
+
+    A test::
+
+        sage: sig == sig0
+        True
+
     """
     sig = []
     if dirname:
@@ -80,6 +150,26 @@ def save_signal(sig, filename, dirname=None):
     - ``filename`` -- string; name of the file
     - ``dirname`` -- (default: None) string; name of directory where the file
       is located
+
+    EXAMPLES::
+
+        sage: from kerrgeodesic_gw import save_signal, read_signal
+        sage: sig = [(RDF(i/10), RDF(sin(i/5))) for i in range(5)]
+        sage: sig   # tol 1.0e-13
+        [(0.0, 0.0),
+         (0.1, 0.19866933079506122),
+         (0.2, 0.3894183423086505),
+         (0.3, 0.5646424733950354),
+         (0.4, 0.7173560908995228)]
+        sage: from sage.misc.temporary_file import tmp_filename
+        sage: filename = tmp_filename(ext='.dat')
+        sage: save_signal(sig, filename)
+
+    A test::
+
+        sage: sig1 = read_signal(filename)
+        sage: sig1 == sig
+        True
 
     """
     if dirname:
